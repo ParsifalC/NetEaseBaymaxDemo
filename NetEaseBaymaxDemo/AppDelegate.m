5e67f632865d56f9d589de2e2b8d9582921ea6bc
Parsifal <zmw@izmw.me>
2017-03-15 22:41:14 +0800

    Move to Unit Test.

--- NetEaseBaymaxDemo/AppDelegate.m
+++ NetEaseBaymaxDemo/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
 
 @interface AppDelegate ()
 @end
@@ -16,63 +15,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    //1、Test unrecognize selector
-    NSObject *obj = [NSObject new];
-    [obj performSelector:@selector(aaaa)];
-    
-    //2、Test KVO crash
-    UIViewController *vc = self.window.rootViewController;
-    //2-1:Cannot remove an observer <...> for the key path "keypath" from <...> because it is not registered as an observer.
-    [vc removeObserver:self forKeyPath:@"title" context:nil];
-    [vc removeObserver:self forKeyPath:@"title" context:nil];
-    [vc addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionInitial context:nil];
-    
-    //2-2:message sent to deallocated instance
-    ViewController *avc = [ViewController new];
-    avc.title = @"avc";
-    [vc addObserver:avc forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:nil];
-    
-    //2-3:deallocated while key value observers were still registered with it.
-    ViewController *bvc = [ViewController new];
-    bvc.title = @"bvc";
-    [bvc addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:nil];
-    
-    //3、Test NSNotification crash -- need iOS9 earlier system
-    [[NSNotificationCenter defaultCenter] addObserver:avc selector:@selector(didReceivedNoti:) name:@"Noti" object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:bvc selector:@selector(didReceivedNoti:) name:@"Noti" object:nil];
-    
-    //4、Test NSTimer crash
-    ViewController *cvc = [ViewController new];
-    cvc.title = @"cvc";
-    [NSTimer scheduledTimerWithTimeInterval:1 target:cvc selector:@selector(fireTimer:) userInfo:@"I'm userInfo" repeats:YES];
-    
-    //5、Test Container crash
-    //5-1:__NSArrayM
-    NSMutableArray *mArray = [NSMutableArray new];
-    [mArray addObject:nil];
-    NSLog(@"%@", mArray[2]);
-    
-    //5-2:__NSArray0
-    NSArray *aArray = @[];
-    NSLog(@"%@", aArray[2]);
-    
-    //5-3:
-    NSArray *bArray = @[@1];
-    NSLog(@"%@", bArray[2]);
-    
-    //5-4:__NSArrayI
-    NSArray *cArray = @[@1, @2];
-    NSLog(@"%@", cArray[3]);
-    
-    //6、Test String crash
-    NSString *astr = @"string";
-    [astr substringFromIndex:astr.length];
-    
     return YES;
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
-    NSLog(@"%@", change);
 }
 
 @end
--- NetEaseBaymaxDemoTests/NetEaseBaymaxDemoTests.m
+++ NetEaseBaymaxDemoTests/NetEaseBaymaxDemoTests.m
@@ -7,9 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "NSObject+Baymax.h"
 
 @interface NetEaseBaymaxDemoTests : XCTestCase
-
 @end
 
 @implementation NetEaseBaymaxDemoTests
@@ -22,7 +22,84 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)Disable_testUnrecognizeSelector {
+    //1、Test unrecognize selector
+    NSObject *obj = [NSObject new];
+    XCTAssertNoThrow([obj performSelector:@selector(crash)]);
+}
+
+- (void)testKVO {
+    //2、Test KVO crash
+    //2-1:Cannot remove an observer <...> for the key path "keypath" from <...> because it is not registered as an observer.
+    UIViewController *avc = [UIViewController new];
+    XCTAssertNoThrow([avc removeObserver:self forKeyPath:@"title" context:nil]);
+    XCTAssertNoThrow([avc removeObserver:self forKeyPath:@"title" context:nil]);
+
+    //2-2:message sent to deallocated instance
+    UIViewController *bvc = [UIViewController new];
+    [bvc addObserver:avc forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:nil];
+
+    //2-3:deallocated while key value observers were still registered with it.
+    UIViewController *cvc = [UIViewController new];
+    [cvc addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+- (void)testNSNotification {
+    //3、Test NSNotification crash -- need iOS9 earlier system
+    NSObject *obj = [NSObject new];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:obj
+                                             selector:@selector(notificationReceived:)
+                                                 name:@"Noti"
+                                               object:nil];
+    
+    XCTAssertTrue(obj.didRegisteredNotificationCenter);
+    
+    typeof(obj) __weak weakObj = obj;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        XCTAssertNil(weakObj);
+    });
+}
+
+- (void)testNSTimer {
+    //4、Test NSTimer crash
+    NSObject *obj = [NSObject new];
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:obj selector:@selector(fireTimer:) userInfo:@"I'm userInfo" repeats:YES];
+    
+    typeof(obj) __weak weakObj = obj;
+    typeof(timer) __weak weakTimer = timer;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        XCTAssertNil(weakObj);
+        XCTAssertNil(weakTimer);
+    });
+}
+
+- (void)testArray {
+    //5、Test Container crash
+    //5-1:__NSArrayM
+    NSMutableArray *mArray = [NSMutableArray new];
+    id nidObj = nil;
+    XCTAssertNoThrow([mArray addObject:nidObj]);
+    XCTAssertNoThrow(mArray[2]);
+    
+    //5-2:__NSArray0
+    NSArray *aArray = @[];
+    XCTAssertNoThrow(aArray[2]);
+    
+    //5-3:
+    NSArray *bArray = @[@1];
+    XCTAssertNoThrow(bArray[2]);
+    
+    //5-4:__NSArrayI
+    NSArray *cArray = @[@1, @2];
+    XCTAssertNoThrow(cArray[3]);
+}
+
+- (void)testString {
+    //6、Test String crash
+    NSString *astr = @"string";
+    XCTAssertNoThrow([astr substringFromIndex:astr.length]);
 }
 
 - (void)testPerformanceExample {
