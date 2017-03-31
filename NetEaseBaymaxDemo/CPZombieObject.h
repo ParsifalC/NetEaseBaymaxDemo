390983553d9740a67f7a5bb76d18b2b169fe0b9f
Parsifal <zmw@izmw.me>
2017-03-31 14:37:04 +0800

    add CPZombieObject.

---
+++ NetEaseBaymaxDemo/CPZombieObject.h
@@ -0,0 +1,13 @@
+//
+//  CPZombieObject.h
+//  NetEaseBaymaxDemo
+//
+//  Created by Parsifal on 2017/3/31.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPZombieObject : NSObject
+
+@end
---
+++ NetEaseBaymaxDemo/CPZombieObject.m
@@ -0,0 +1,13 @@
+//
+//  CPZombieObject.m
+//  NetEaseBaymaxDemo
+//
+//  Created by Parsifal on 2017/3/31.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+#import "CPZombieObject.h"
+
+@implementation CPZombieObject
+
+@end
