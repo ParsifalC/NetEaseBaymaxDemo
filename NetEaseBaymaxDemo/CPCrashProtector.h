7963f2b009d3dc3881cea0300d8dc4109b2a9669
Parsifal <zmw@izmw.me>
2017-04-03 21:02:58 +0800

    add new file.

---
+++ NetEaseBaymaxDemo/CPCrashProtector.h
@@ -0,0 +1,13 @@
+//
+//  CPCrashProtector.h
+//  NetEaseBaymaxDemo
+//
+//  Created by Parsifal on 2017/4/3.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPCrashProtector : NSObject
+
+@end
---
+++ NetEaseBaymaxDemo/CPCrashProtector.m
@@ -0,0 +1,13 @@
+//
+//  CPCrashProtector.m
+//  NetEaseBaymaxDemo
+//
+//  Created by Parsifal on 2017/4/3.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+#import "CPCrashProtector.h"
+
+@implementation CPCrashProtector
+
+@end
